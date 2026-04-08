9a4bfcaee18d6d17b6e372e14f675709d0c71b52
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-04-08 17:38:41 +0500

    hello

--- 38_with_statement.py
+++ 38_with_statement.py
@@ -14,3 +14,16 @@ f.close()
 
 with open("file.txt") as f:
     print(f.read())
+
+
+
+
+a = int(input("enter the age when you join the school  :"))
+
+if(a>=18):
+    print("yes sir you are the best for all")
+
+else:
+    print("you are not that good to join the school")    
+
+print("the program has executed successfully")  
