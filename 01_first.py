322a796b6cdbf9e22cb035c3c0ea057a40b3240b
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-07-24 12:29:35 +0500

    Update 01_first.py

--- 01_first.py
+++ 01_first.py
@@ -4,6 +4,7 @@ print ("hello world")
 "This is my third commit"
 "This is my fourth commit"
 "This is my fifth commit"
+process.exit(1);
 #line is commited
 print ("hello world")
 "This is my first commit"
